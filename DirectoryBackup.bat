88857308d073706a0f78e6536d92f4dd8f094875
kaiyamashita <kaiyamashita@kknbs.co.jp>
2025-09-09 14:05:32 +0900

    リファクタリング

--- DirectoryBackup.bat
+++ DirectoryBackup.bat
@@ -1,7 +1,7 @@
 rem @echo off
 cd %~dp0
 
-set /p Suffix=Input BackupDirectory Suffix
+set /p Suffix=Input BackupDirectory Suffix : 
 
 PowerShell -ExecutionPolicy Unrestricted -File ".\DirectoryBackup.ps1" -CsvFilePath ".\DirectoryBackup.csv" -Suffix %Suffix%
 
